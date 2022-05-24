eafbfffe392b0bcbc72fdab7cfb0ccfb254b4422
Noelia Figueiras Fernández <noelia.figueiras@vigo.org>
2022-05-24 12:23:42 +0200

    laravel blog mayo

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Contenido de Contacto</h1>
+
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+
+<h1>Contenido de Home</h1>
+
+
+@endsection
